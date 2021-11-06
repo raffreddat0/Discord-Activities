02ccaad366c70f5eb24ebc1222d6d00367a50bf5
Raffreddat07 <91798386+Raffreddat07@users.noreply.github.com>
2021-11-06 14:11:02 +0100

    Update DiscordActivities.plugin.js

--- DiscordActivities.plugin.js
+++ DiscordActivities.plugin.js
@@ -332,20 +332,21 @@ function buildPlugin([BasePlugin, PluginApi]) {
 			}
 			enableExperiment() {
 				const app = {
-  				youtube: '880218394199220334', // Note : First package to include the new YouTube Together version, any other package offering it will be clearly inspired by it
-  				youtubedev: '880218832743055411', // Note : First package to include the new YouTube Together development version, any other package offering it will be clearly inspired by it
-  				poker: '755827207812677713',
-  				betrayal: '773336526917861400',
-  				fishing: '814288819477020702',
-  				chess: '832012774040141894',
-  				chessdev: '832012586023256104', // Note : First package to offer chessDev, any other package offering it will be clearly inspired by it
-  				lettertile: '879863686565621790', // Note : First package to offer lettertile, any other package offering it will be clearly inspired by it
-  				wordsnack: '879863976006127627', // Note : First package to offer wordsnack any other package offering it will be clearly inspired by it
-  				doodlecrew: '878067389634314250', // Note : First package to offer doodlecrew, any other package offering it will be clearly inspired by it
-  				awkword: '879863881349087252', // Note : First package to offer awkword, any other package offering it will be clearly inspired by it
-  				spellcast: '852509694341283871', // Note : First package to offer spellcast, any other package offering it will be clearly inspired by it
+  					youtube: '880218394199220334', // Note : First package to include the new YouTube Together version, any other package offering it will be clearly inspired by it
+  					youtubedev: '880218832743055411', // Note : First package to include the new YouTube Together development version, any other package offering it will be clearly inspired by it
+  					poker: '755827207812677713',
+  					betrayal: '773336526917861400',
+  					fishing: '814288819477020702',
+  					chess: '832012774040141894',
+  					chessdev: '832012586023256104', // Note : First package to offer chessDev, any other package offering it will be clearly inspired by it
+  					lettertile: '879863686565621790', // Note : First package to offer lettertile, any other package offering it will be clearly inspired by it
+  					wordsnack: '879863976006127627', // Note : First package to offer wordsnack any other package offering it will be clearly inspired by it
+  					doodlecrew: '878067389634314250', // Note : First package to offer doodlecrew, any other package offering it will be clearly inspired by it
+  					awkword: '879863881349087252', // Note : First package to offer awkword, any other package offering it will be clearly inspired by it
+  					spellcast: '852509694341283871', // Note : First package to offer spellcast, any other package offering it will be clearly inspired by it
+					checkers: '832013003968348200' // Note : First package to offer checkers, any other package offering it will be clearly inspired by it
 				};
-				const applicationIds = [app.poker, app.chess, app.betrayal, app.fishing, app.lettertile, app.wordsnack, app.doodlecrew, app.awkword, app.spellcast, app.youtube];
+				const applicationIds = [app.poker, app.chess, app.checkers, app.betrayal, app.fishing, app.lettertile, app.wordsnack, app.doodlecrew, app.awkword, app.spellcast, app.youtube];
 				external_PluginApi_namespaceObject.Patcher.instead(activitiesExperiment, "getEnabledAppIds", (function() {
 					return applicationIds;
 				}));
